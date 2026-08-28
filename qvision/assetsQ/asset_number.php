a2d744eb0bdc5888e128231b9cb1be6baf53bdaf
Vaithi <vaithi663@gmail.com>
2026-08-28 11:21:50 +0530

    stock_number increse fixed

--- qvision/assetsQ/asset_number.php
+++ qvision/assetsQ/asset_number.php
@@ -4,44 +4,42 @@ $asset_id=$_REQUEST['id'];
 $asset=$_REQUEST['asset'];
 $asset_type=$_REQUEST['asset_type'];
 
-$sql=$con->query("select * from assets_form_detail where asset='$asset' and asset_type='$asset_type' and asset_name='$asset_id' order by id desc limit 1");
+// First get the prefix from master table
+$asset_query=$con->query("select * from assets_master where id='$asset_id'");
+$asfet=$asset_query->fetch();
+$prefix=$asfet['prefix_code'];
 
-//echo "select * from assets_form_detail where asset='$asset' and asset_type='$asset_type' and asset_name='$asset_id' order by id desc limit 1";
+// Now search using the 'prefix' column instead of asset_name ID, and order by the highest asset_no
+$sql=$con->query("select * from assets_form_detail where prefix='$prefix' order by CAST(asset_no AS UNSIGNED) desc limit 1");
 $cou=$sql->rowCount();
 
-
-$asset=$con->query("select * from assets_master where id='$asset_id'");
-$asfet=$asset->fetch();
-$prefix=$asfet['prefix_code'];
 if($cou==0)
 {	
-$no=$prefix."-".'0001';	
-echo $no;
+    $no=$prefix."-".'0001';	
+    echo $no;
 }
 else
 {
-	$sqlfet=$sql->fetch();
- $asset_no=$sqlfet['asset_no'];
-	$num=$asset_no+1;
-	$len=strlen($num);
-	if($len==1)
-	{
-		echo $no=$prefix."-".'000'.$num;
-	}
-	elseif($len==2)
-	{
-		echo $no=$prefix."-".'00'.$num;
-	}
-	elseif($len==3)
-	{
-		echo $no=$prefix."-".'0'.$num;
-	}
-	elseif($len==4)
-	{
-		echo $no=$prefix."-".$num;
-	}
-	
-	
+    $sqlfet=$sql->fetch();
+    $asset_no=$sqlfet['asset_no'];
+    $num=$asset_no+1;
+    $len=strlen($num);
+    
+    if($len==1)
+    {
+        echo $no=$prefix."-".'000'.$num;
+    }
+    elseif($len==2)
+    {
+        echo $no=$prefix."-".'00'.$num;
+    }
+    elseif($len==3)
+    {
+        echo $no=$prefix."-".'0'.$num;
+    }
+    elseif($len==4)
+    {
+        echo $no=$prefix."-".$num;
+    }
 }
-
 ?>
